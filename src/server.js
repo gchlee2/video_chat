fd41b803b88acfb73f673a4ade9f4d88f4a4ba80
gchlee2 <gchlee2@naver.com>
2021-10-01 21:53:33 +0900

    헤로쿠 포트 수정

--- src/server.js
+++ src/server.js
@@ -13,5 +13,6 @@ app.get("/*", (_, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-httpServer.listen(3000, handleListen);
+console.log(process.env.PORT);
+const handleListen = () => console.log(`Listening on http://localhost:3000  or https://plecon.herokuapp.com/`);
+httpServer.listen(process.env.PORT || 3000, handleListen);
